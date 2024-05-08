9def410fbce4b3432cee168db491c76082d5bfa4
ShadowArcanist <projectshadowmoon@gmail.com>
2024-05-08 21:44:45 +0530

    ✨ Added Dynamic OG image generation
    > When sharing any links to our docs pages on social media
    it won't show any OG image, it only shows title & description.
    
    > I have fixed this OG image issue by adding Vercel's OG library
    which auto-generates OG image based on the title of the page.
    
    > This OG generation uses Vercel Edge runtime.

---
+++ pages/api/og.jsx
@@ -0,0 +1,82 @@
+/* eslint-env node */
+import { ImageResponse } from '@vercel/og'
+
+export const config = {
+  runtime: 'edge'
+}
+
+const font = fetch(new URL('./Inter-SemiBold.otf', import.meta.url)).then(res =>
+  res.arrayBuffer()
+)
+
+export default async function (req) {
+  const inter = await font
+
+  const { searchParams } = new URL(req.url)
+
+  // ?title=<title>
+  const hasTitle = searchParams.has('title')
+  const title = hasTitle
+    ? searchParams.get('title')?.slice(0, 100)
+    : 'AnythingLLM'
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+          padding: 80,
+          backgroundColor: '#030303',
+          backgroundImage: 'radial-gradient(circle at 25px 25px, #333 2%, transparent 0%), radial-gradient(circle at 75px 75px, #333 2%, transparent 0%)',
+          backgroundSize: '100px 100px',
+          backgroundPosition: '-30px -10px',
+          fontWeight: 600,
+          color: 'white'
+        }}
+      >
+        <p
+          style={{
+            position: 'absolute',
+            bottom: 70,
+            left: 80,
+            margin: 0,
+            fontSize: 30,
+            letterSpacing: -1
+          }}
+        >
+          AnythingLLM | The all-in-one AI desktop app.
+        </p>
+        <h1
+          style={{
+            fontSize: 82,
+            margin: '0 0 40px -2px',
+            lineHeight: 1.1,
+            textShadow: '0 2px 30px #000',
+            letterSpacing: -4,
+            backgroundImage: 'linear-gradient(90deg, #fff 40%, #aaa)',
+            backgroundClip: 'text',
+            color: 'transparent'
+          }}
+        >
+          {title}
+        </h1>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: 'inter',
+          data: inter,
+          style: 'normal'
+        }
+      ]
+    }
+  )
+}
